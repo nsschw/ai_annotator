dc0fc6e050983caee151c0d1c68d27bc79f0a668
Poltergeist <77846583+nmeisters@users.noreply.github.com>
2024-09-24 16:28:53 +0200

    Refactor imports in __init__.py: update module paths for Model and HuggingFaceEmbeddingModel

--- ai_annotator/core/__init__.py
+++ ai_annotator/core/__init__.py
@@ -1,5 +1,5 @@
 from .annotation_project import AnnotationProject
-from .models import Model, OpenAIModel
-from .embedding_models import HuggingFaceEmbeddingModel
+from .model import Model, OpenAIModel
+from .embedding_model import HuggingFaceEmbeddingModel
 
 __all__ = ["AnnotationProject", "OpenAIModel", "HuggingFaceEmbeddingModel"]
