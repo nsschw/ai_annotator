80fb320d76b280aabea22d12308fd0d5f7b24d81
nsschw <s2nsschw@uni-trier.de>
2024-09-27 17:58:40 +0200

    Add comment to PathConfig class indicating pip installation issue

--- src/ai_annotator/core/config.py
+++ src/ai_annotator/core/config.py
@@ -48,6 +48,10 @@ class AnnotationConfig():
 
 
 class PathConfig:
+    """
+    Won't work if pip installed
+    """
+
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     PROMPTS_DIR = os.path.join(BASE_DIR, 'prompts')
     GOLD_LABEL_PROMPT = os.path.join(PROMPTS_DIR, 'gold_label-induced_reasoning.txt')
